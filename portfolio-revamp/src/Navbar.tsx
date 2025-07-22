cef2a3ab4e8875508281dc32c6c56a73cfc93fa9
tasmiachow <tasmiachowdhury123@gmail.com>
2025-07-22 15:55:32 -0400

    react setup

---
+++ portfolio-revamp/src/Navbar.tsx
@@ -0,0 +1,27 @@
+
+
+function Navbar(){
+    return(
+        <>
+            <nav className="navbar">
+                <ul> 
+                    <li>
+                        <a href = "/about"> About </a>
+                    </li>
+                    <li>
+                        <a href = "/about"> Experience </a>
+                    </li>
+                    <li>
+                        <a href = "/about"> Projects </a>
+                    </li>
+                    <li>
+                        <a href = "/about"> Contact </a>
+                    </li>
+                </ul>
+
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
